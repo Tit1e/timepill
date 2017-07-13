571d8f7a45d0df970debed5a477ff8731cc94df4
Tit1e <evollyone@outlook.com>
2017-07-13 23:49:10 +0800

    胶囊日记chrome插件V2.0
    胶囊日记chrome插件V2.0发布

--- README.md
+++ README.md
@@ -3,14 +3,23 @@
 本插件目前只适用于chrome浏览器，其他使用chrome浏览器内核的第三方浏览器可能可以使用，本人未测试。
 本插件为 <a href="http://www.timepill.net">胶囊日记</a> 第三方插件，写这个插件的初衷是为了能更加方便地刷胶囊= =。
 ## 版本
-胶囊日记chrome插件V1.0
+* 胶囊日记chrome插件V1.0
+    + 胶囊日记chrome插件V1.0发布
+* 胶囊日记chrome插件V2.0
+    + 修改展现方式为注入页面中，这样切换标签不会使页面消失，通过再次点击插件图标关闭窗口，并且可以切换日记本。
 ## 预览
+V1.0预览
 <center>
 
 ![](https://user-images.githubusercontent.com/17524418/28151806-93111a08-67cf-11e7-9ef1-ae5455e9c145.png)
 
 </center>
+V2.0预览
+<center>
 
+![](https://user-images.githubusercontent.com/17524418/28174974-940db172-6825-11e7-815c-7f5d30f5ba28.png)
+
+</center>
 ## 使用方法
 
 1.在chrome浏览器地址输入 chrome://extensions/ 进入扩展管理页面。
---
+++ background.js
@@ -0,0 +1,25 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	  // No tabs or host permissions needed!
+	  chrome.tabs.executeScript({
+                  code: '\
+                        if(document.getElementById("ifr")){\
+                                var ifr = document.getElementById("ifr");\
+                                document.body.removeChild(ifr);\
+                        }else{\
+                                var iframe = document.createElement("iframe");\
+                                iframe.setAttribute("src", "https://m.timepill.net");\
+                                iframe.setAttribute("frameborder", "0");\
+                                iframe.setAttribute("id", "ifr");\
+                                iframe.setAttribute("width", "360px");\
+                                iframe.setAttribute("height", "580px");\
+                                iframe.style.position = "fixed";\
+                                iframe.style.top = "30px";\
+                                iframe.style.right = "30px";\
+                                iframe.style.boxShadow = "0px 2px 10px rgba(0,0,0,0.3)";\
+                                iframe.style.border = "1px solid #eee";\
+                                iframe.style.zIndex = "9999";\
+                                document.body.append(iframe)\
+                        }\
+                '
+	  });
+	});
